a5b24081999b7613e28fca22211a6a16fc150d72
prempunith <104521021+pprempunith@users.noreply.github.com>
2024-02-28 23:07:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ for ( let i = 0 ; i<numberOfdrums ; i++ ){
         
         makeSound(buttonInnerHTML);
 
+        buttonAnimation (buttonInnerHTML)
     });
 }
 
 document.addEventListener("keydown", function(event) {
 makeSound(event.key );
+
+buttonAnimation(event.key)
 });
 
 function makeSound(key){
@@ -57,6 +60,18 @@ function makeSound(key){
             break;
     }
 
+}
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){
+        activeButton.classList.remove("pressed");
+
+    }, 100)
 }
 //
 
